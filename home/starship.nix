6bd1f0a418a6157acd54c07f93e1cc355a18e484
Michael Zuercher <michzuerch@gmail.com>
2024-01-10 11:46:12 +0100

    starship with bun and new break line

--- home/starship.nix
+++ home/starship.nix
@@ -1,82 +1,93 @@
 { pkgs, lib, ... }: {
   programs.starship = {
-      enable = true;
-      settings = {
-        add_newline = false;
+    enable = true;
+    settings = {
+      add_newline = true;
+      format = lib.concatStrings [
+        "[░▒▓](#a3aed2)"
+        "[  ](bg:#a3aed2 fg:#090c0c)"
+        "[](bg:#769ff0 fg:#a3aed2)"
+        "$directory"
+        "[](fg:#769ff0 bg:#394260)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:#394260 bg:#212736)"
+        "$bun"
+        "$nodejs"
+        "$rust"
+        "$golang"
+        "$php"
+        "[](fg:#212736 bg:#1d2230)"
+        "$time"
+        "[ ](fg:#1d2230)"
+	      "$sudo"
+	      "$line_break"
+        "$character"
+      ];
 
-        format = lib.concatStrings [
-          "[░▒▓](#a3aed2)"
-          "[  ](bg:#a3aed2 fg:#090c0c)"
-          "[](bg:#769ff0 fg:#a3aed2)"
-          "$directory"
-          "[](fg:#769ff0 bg:#394260)"
-          "$git_branch"
-          "$git_status"
-          "[](fg:#394260 bg:#212736)"
-          "$nodejs"
-          "$rust"
-          "$golang"
-          "$php"
-          "[](fg:#212736 bg:#1d2230)"
-          "$time"
-          "[ ](fg:#1d2230)"
-	  "$sudo"
-          "$character"
-        ];
-
-        directory = {
-          style = "fg:#e3e5e5 bg:#769ff0";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            "Documents" = "󰈙 ";
-            "Downloads" = " ";
-            "Music" = " ";
-            "Pictures" = " ";
-	  };
-        };
-
-        git_branch = {
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-        };
-
-        git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-	};
-
-	nodejs = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        rust = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        golang = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-	};
-
-        time = {
-           disabled = false;
-           time_format = "%R"; # Hour:Minute Format
-           style = "bg:#1d2230";
-           format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-        };
-
-        sudo = {
-           disabled = false;
-           style = "bg:#1d2230";
-        };
+      directory = {
+        style = "fg:#e3e5e5 bg:#769ff0";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+        substitutions = {
+          "Documents" = "󰈙 ";
+          "Downloads" = " ";
+          "Music" = " ";
+          "Pictures" = " ";
+	      };
       };
+
+      git_branch = {
+        symbol = "";
+        style = "bg:#394260";
+        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+      };
+
+      git_status = {
+        style = "bg:#394260";
+        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+	    };
+
+      nodejs = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+
+      bun = {
+        symbol = "🍔";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      
+      rust = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+
+      golang = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+
+      time = {
+        disabled = false;
+        time_format = "%R"; # Hour:Minute Format
+        style = "bg:#1d2230";
+        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+      };
+
+      sudo = {
+        disabled = false;
+        style = "bg:#1d2230";
+      };
+
+      line_break = {
+        disabled = false;
+      };
+    };
   };
 }
