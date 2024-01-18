60f755f95be24fa074def480ecd39dc68722cdab
Michael Zuercher <michzuerch@gmail.com>
2024-01-18 17:48:16 +0100

    Stable version 23.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "nixos michzuerch 2024";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
