acc42d73a30f4c290c33a6cdb49d354846033c3c
Michael Zuercher <michzuerch@gmail.com>
2024-01-23 11:54:02 +0100

    module in system for network configuration

--- flake.nix
+++ flake.nix
@@ -33,18 +33,18 @@
           #./system/cockroach.nix
           #./system/hacking.nix
           ./system/fonts.nix
-	        ./system/powermanagement.nix
-	        #./system/resolvedonly.nix
+          ./system/powermanagement.nix
+          ./system/networking.nix
           ./system/virtualisation.nix
-	        ./system/xdg.nix
-	        ./system/zsh.nix
+          ./system/xdg.nix
+          ./system/zsh.nix
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
-	          home-manager.useGlobalPkgs = true;
+            home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {inherit inputs;}; 
-	          home-manager.users.michzuerch = import ./home/home.nix;
+            home-manager.users.michzuerch = import ./home/home.nix;
           }
         ];
       };
