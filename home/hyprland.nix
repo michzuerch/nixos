89d50f2e59d9d5eae43e09166881cfbdf05ffb43
Michael Zuercher <michzuerch@gmail.com>
2024-03-01 17:28:12 +0100

    hyprland windowrules etc

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -24,6 +24,34 @@ in
     plugins = [];
     settings = {
       exec-once = ''${startup-script}/bin/start'';
+      monitor = ",preferred,auto,1";
+      xwayland = {
+        force_zero_scaling = true;
+      };
+      input = {
+        kb_layout = "us";
+        repeat_delay = 350;
+        repeat_rate = 50;
+        accel_profile = "adaptive";
+        follow_mouse = 2;
+      };
+      general = {
+        layout = "dwindle";
+        gaps_in = 7;
+        gaps_out = 7;
+        border_size = 4;
+        resize_on_border = true;
+        no_cursor_warps = false;
+        no_border_on_floating = true;
+        cursor_inactive_timeout = 30;
+      };
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        mouse_move_enables_dpms = true;
+        enable_swallow = true;
+        swallow_regex = "^(alacritty)$";
+      };
       decoration = {
         rounding = 8;
         active_opacity = 1.0;
@@ -42,6 +70,66 @@ in
         shadow_render_power = 2;
         col.shadow = "0x66000000";
       };
+      animations = {
+        enabled = true;
+        bezier = [
+          "overshot, 0.05, 0.9, 0.1, 1.05"
+          "smoothOut, 0.36, 0, 0.66, -0.56"
+          "smoothIn, 0.25, 1, 0.5, 1"
+        ];
+        animation = [
+          "windows, 1, 5, overshot, slide"
+          "windowsOut, 1, 4, smoothOut, slide"
+          "windowsMove, 1, 4, default"
+          "border, 1, 10, default"
+          "fade, 1, 10, smoothIn"
+          "fadeDim, 1, 10, smoothIn"
+          "workspaces, 1, 6, default"
+        ];
+      };
+      dwindle = {
+        no_gaps_when_only = false;
+        pseudotitle = true;
+        preserve_split = true;
+      };
+      windowrule = [
+        "float, file_progress"
+        "float, confirm"
+        "float, dialog"
+        "float, download"
+        "float, notification"
+        "float, error"
+        "float, splash"
+        "float, confirmreset"
+        "float, title:Open File"
+        "float, title:branchdialog"
+        "float, Lxappearance"
+        "float, Rofi"
+        "animation none,Rofi"
+        "float,viewnior"
+        "float,feh"
+        "float, pavucontrol-qt"
+        "float, pavucontrol"
+        "float, file-roller"
+        "fullscreen, wlogout"
+        "float, title:wlogout"
+        "fullscreen, title:wlogout"
+        "idleinhibit fullscreen, firefox"
+        "float, title:^(Media viewer)$"
+        "float, title:^(Volume Control)$"
+        "float, title:^(Picture-in-Picture)$"
+        "size 800 600, title:^(Volume Control)$"
+        "move 75 44%, title:^(Volume Control)$"
+        "float, ^(blueberry.py)$"
+      ];
+      windowsrulev2 = [
+        "float,class: ^(pavucontrol)$"
+        "size 86% 40%, class:^(pavucontrol)$"
+        "move 50% 6%, class:^(pavucontrol)$"
+        "workspace special silent,$class:^(pavucontrol)$"
+        "opacity 0.80,$class:^(pavucontrol)$"
+        "float,title:^(Kdenlive)$"
+      ];
       "$mod" = "SUPER";
       bindm = [
         "$mod, mouse:272, movewindow"
@@ -103,7 +191,6 @@ in
     wlr-randr
     wlsunset
     wofi-emoji
-    wtype
     wtype
     xdg-desktop-portal
     xdg-desktop-portal-gtk
