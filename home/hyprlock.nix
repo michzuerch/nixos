f17cc161500f33c22a2db47e34cb8ae8b875e507
Michael Zuercher <michzuerch@gmail.com>
2024-03-11 17:45:59 +0100

    better hyprlock

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -12,46 +12,56 @@
     monitor =
     path = $HOME/Wallpapers/hyprlock.png  # only png supported for now
     color = rgba(25, 20, 20, 1.0)
-
-    # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
-    blur_passes = 4 # 0 disables blurring
-    blur_size = 2
+    blur_passes = 3 # 0 disables blurring
+    blur_size = 4
     noise = 0.0117
-    contrast = 0.8916
-    brightness = 0.8172
-    vibrancy = 0.1696
+    contrast = 1.3000
+    brightness = 0.8000
+    vibrancy = 0.2100
     vibrancy_darkness = 0.0
   }
 
   input-field {
     monitor =
-    size = 200, 50
-    outline_thickness = 1
+    size = 250, 50
+    outline_thickness = 3
     dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
-    dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
+    dots_spacing = 0.64 # Scale of dots' absolute size, 0.0 - 1.0
     dots_center = true
     outer_color = rgb(000000)
     inner_color = rgb(200, 200, 200)
     font_color = rgb(10, 10, 10)
     fade_on_empty = true
-    placeholder_text = <i>Input Password...</i> # Text rendered in the input box when it's empty.
+    placeholder_text = <i>Password...</i> # Text rendered in the input box when it's empty.
     hide_input = false
-    position = 0, -20
+    position = 0, 150
     halign = center
     valign = center
   }
 
   label {
     monitor =
-    text = Enter your password to unlock
-    color = rgba(200, 200, 200, 1.0)
-    font_size = 25
-    font_family = Noto Sans
-
-    position = 0, 200
+    text = cmd[update:1000] echo "<b><big> $(date +"%H:%M:%S") </big></b>"
+    color = rbga(200, 200, 200, 1.0)
+    font_size = 64
+    font_family = JetBrains Mono Nerd Font 10
+    position = 0, 16
     halign = center
     valign = center
   }
+
+  label {
+    monitor =
+    text = Hey <span text_transform="capitalize" size="larger">$USER</span>
+    color = $color0
+    font_size = 20
+    font_family = JetBrains Mono Nerd Font 10
+    position = 0, 0
+    halign = center
+    valign = center
+  }
+
+
   '';
 
   home.packages = with pkgs; [
