0528fb641e6c87770b380112d4d19815c53064bd
Michael Zuercher <michzuerch@gmail.com>
2024-04-13 10:01:22 +0200

    cachix for hyprland

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -7,6 +7,8 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       trusted-users = [ "root" "michzuerch" ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
   };
 }
