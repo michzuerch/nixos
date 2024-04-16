09e19d48db7c1bf3d4e5786cdf49156e0cf5acd0
Michael Zuercher <michzuerch@gmail.com>
2024-04-16 13:32:26 +0200

    public key user for openssh

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,7 @@
         options = "";
       };
       libinput.enable = true;
+      # Default for nixos?
       # displayManager = {
       #   lightdm = {
       #     enable = true;
@@ -162,6 +163,9 @@
     description = "Michi";
     shell = pkgs.zsh;
     extraGroups = ["networkmanager" "wheel" "tss" "video" "wireshark" "podman"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILsyEfMjNUlwrf4NG3f6BWpP4uSzCfpC7V5jMqtiLfSQ michzuerch@localhost"
+    ];
   };
   environment = {
     pathsToLink = ["/libexec"];
