1c09d77831a22ef03724b3e25bc87597ac8a185b
Michael Zuercher <michzuerch@gmail.com>
2024-04-25 12:08:33 +0200

    new nixvim

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -1,5 +1,17 @@
 {pkgs, ...}: {
   programs.nixvim = {
     enable = true;
+    enableMan = true;
+    options = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+
+    };
+    plugins = {
+      lightline.enable = true;
+
+    };
+    colorschemes.gruvbox.enable  = true;
   };
 }
