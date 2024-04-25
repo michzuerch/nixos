7af2843d9146c954f03c3c0e57a01be2d4aa895b
Michael Zuercher <michzuerch@gmail.com>
2024-04-25 13:37:17 +0200

    nixvim config from vimjoyer

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -8,10 +8,30 @@
       shiftwidth = 2;
 
     };
+    globals.mapleader = " ";
     plugins = {
       lightline.enable = true;
 
     };
     colorschemes.gruvbox.enable  = true;
+    keymaps = [
+      {
+	action = "<cmd>Telescope live_grep<CR>";
+	key = "<leader>g";
+      }
+    ];
+    autoCmd = [
+      {
+	event = [ "BufEnter" "BufWinEnter" ];
+	pattern = [ "*.c" "*.h" ];
+	command = "echo 'Entering a c or c++ file'";
+      }
+    ];
+    highlight = {
+      Comment.fg = "#ff00ff";
+      Comment.bg = "#000000";
+      Comment.underline = true;
+      Commend.bold = true;
+    };
   };
 }
