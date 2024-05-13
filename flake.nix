7fde83a423028b394b487b5fcb5cc86386f138fe
Michael Zuercher <michzuerch@gmail.com>
2024-05-13 16:57:47 +0200

    working version

--- flake.nix
+++ flake.nix
@@ -15,7 +15,8 @@
       url = "github:NixOS/nixos-hardware/master";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland";
+      #url = "github:hyprwm/Hyprland";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-plugins = {
--- home/node.nix
+++ home/node.nix
@@ -6,6 +6,6 @@
 
   home.packages = with pkgs; [
     bun
-    nodejs_21
+    nodejs
   ];
 }
