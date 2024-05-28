9e2f7569681a9b0c71311e302541949572e7de7f
Michael Zuercher <michzuerch@gmail.com>
2024-05-28 17:01:13 +0200

    additional packages for neovim

--- home/neovim/neovim.nix
+++ home/neovim/neovim.nix
@@ -4,6 +4,33 @@
     vimAlias = true;
     vimdiffAlias = true;
     withNodeJs = true;
+    extraPackages = with pkgs; [
+          gcc # needed for nvim-treesitter
+
+          # HTML, CSS, JSON
+          vscode-langservers-extracted
+
+          # LazyVim defaults
+          stylua
+          shfmt
+
+          # Markdown extra
+          nodePackages.markdownlint-cli
+          marksman
+
+          # Docker extra
+          nodePackages.dockerfile-language-server-nodejs
+          hadolint
+          docker-compose-language-service
+
+          # JSON and YAML extras
+          nodePackages.yaml-language-server
+
+          # Custom
+          editorconfig-checker
+          shellcheck
+        ];
+
   };
 
   xdg.configFile."nvim" = {
