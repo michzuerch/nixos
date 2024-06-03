eaa89878655c0e53e4df86ca8a18d9eddef2b101
Michael Zuercher <michzuerch@gmail.com>
2024-06-03 14:56:46 +0200

    first test nixvim2

---
+++ system/nixvim2/plugins/themes/default.nix
@@ -0,0 +1,44 @@
+{
+  colorschemes = {
+    catppuccin = {
+      enable = true;
+      settings = {
+        background = {
+          light = "macchiato";
+          dark = "mocha";
+        };
+        flavour = "macchiato"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+        no_bold = false;
+        no_italic = false;
+        no_underline = false;
+        transparent_background = true;
+        integrations = {
+          cmp = true;
+          notify = true;
+          gitsigns = true;
+          neotree = true;
+          which_key = true;
+          illuminate = {
+            enabled = true;
+          };
+          treesitter = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          mini.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
+          };
+        };
+      };
+    };
+  };
+}
