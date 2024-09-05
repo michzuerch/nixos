d4109cb33cd5f96621b936224b053ee43bae6270
Michael Zuercher <michzuerch@gmail.com>
2024-09-05 16:37:21 +0200

    add broot

--- home/sh.nix
+++ home/sh.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs = {
     bash = {
       enable = true;
@@ -13,12 +13,15 @@
     fzf = {
       enable = true;
       catppuccin.enable = true;
-      defaultCommand = "fd --type f --hidden --no-ignore --follow --exclude .git";
+      defaultCommand =
+        "fd --type f --hidden --no-ignore --follow --exclude .git";
       enableZshIntegration = true;
       enableFishIntegration = false;
     };
 
     jq.enable = true;
+
+    broot.enable = true;
 
     gitui.enable = true;
 
