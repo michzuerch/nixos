47b047b39ae4b63f7711855e81704f5ac16f27ef
Michael Zuercher <michzuerch@gmail.com>
2024-09-11 09:08:19 +0200

    remove memtest

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,21 +1,21 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   # Bootloader.
   boot = {
-    # bootspec.enable = true;
-    # tmp.cleanOnBoot = true;
+    bootspec.enable = true;
+    tmp.cleanOnBoot = true;
     consoleLogLevel = 0;
     initrd = {
       enable = true;
       systemd.enable = true;
       verbose = false;
-      availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "sd_mod"];
-      kernelModules = [];
+      availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
     };
-    kernelModules = ["kvm-intel"];
-    extraModulePackages = [];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
     loader = {
       systemd-boot.enable = true;
-      systemd-boot.memtest86.enable = true;
+      # systemd-boot.memtest86.enable = true;
       efi.canTouchEfiVariables = true;
     };
     # plymouth = {
