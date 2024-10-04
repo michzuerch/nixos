dafa8be4d9c9292b775c12d014b39386cede6195
Michael Zuercher <michzuerch@gmail.com>
2024-10-04 13:26:54 +0200

    gtk config from sample HeinzDev

--- home/michzuerch/gtk.nix
+++ home/michzuerch/gtk.nix
@@ -1,50 +1,52 @@
 { pkgs, ... }:
 {
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Flat-Remix-GTK-Grey-Darkest";
-      package = pkgs.flat-remix-gtk;
-    };
-    font = {
-      name = "Ubuntu";
-      size = 12;
-      package = pkgs.ubuntu_font_family;
-    };
-    cursorTheme = {
-      # package = pkgs.gnome.adwaita-icon-theme;
-      package = pkgs.quintom-cursor-theme;
-      # name = "Adwaita";
-      name = "Quintom_Snow"; # Quintom_Ink
-      size = 24;
-    };
-    iconTheme = {
-      name = "Adwaita";
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
-
-  # Sample from Github HeinzDev
   # gtk = {
   #   enable = true;
-  #   iconTheme = {
-  #     name = "Yaru-magenta-dark";
-  #     package = pkgs.yaru-theme;
-  #   };
-  #
   #   theme = {
-  #     name = "Tokyonight-Dark-B-LB";
-  #     package = pkgs.tokyo-night-gtk;
+  #     name = "Flat-Remix-GTK-Grey-Darkest";
+  #     package = pkgs.flat-remix-gtk;
   #   };
-  #
+  #   font = {
+  #     name = "Ubuntu";
+  #     size = 12;
+  #     package = pkgs.ubuntu_font_family;
+  #   };
   #   cursorTheme = {
-  #     name = "Bibata-Modern-Classic";
-  #     package = pkgs.bibata-cursors;
+  #     # package = pkgs.gnome.adwaita-icon-theme;
+  #     package = pkgs.quintom-cursor-theme;
+  #     # name = "Adwaita";
+  #     name = "Quintom_Snow"; # Quintom_Ink
+  #     size = 24;
+  #   };
+  #   iconTheme = {
+  #     name = "Adwaita";
+  #   };
+  #   gtk3.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  #   gtk4.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  # };
+  #
+  # Sample from Github HeinzDev
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Yaru-magenta-dark";
+      package = pkgs.yaru-theme;
+    };
+
+    theme = {
+      name = "Tokyonight-Dark-B-LB";
+      package = pkgs.tokyo-night-gtk;
+    };
+
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+    };
+  };
 
   home = {
     pointerCursor = {
@@ -56,7 +58,7 @@
     };
 
     packages = with pkgs; [
-      gnome.adwaita-icon-theme
+      adwaita-icon-theme
       libsForQt5.qt5ct
     ];
 
