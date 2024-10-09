a9b6c70d2665b4bb304698f9ef0bed8db2425ec4
Michael Zuercher <michzuerch@gmail.com>
2024-10-09 13:24:08 +0200

    new bootleader config

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,28 +1,38 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   # Bootloader.
   boot = {
-    bootspec.enable = true;
-    tmp.cleanOnBoot = true;
+    # bootspec.enable = true;
+    # tmp.cleanOnBoot = true;
     consoleLogLevel = 0;
     initrd = {
       enable = true;
       systemd.enable = true;
       verbose = false;
-      availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "sd_mod"];
-      kernelModules = [];
+      availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
     };
-    kernelModules = ["kvm-intel"];
-    extraModulePackages = [];
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "nowatchdog"
+    ];
+
+    extraModulePackages = [ ];
     loader = {
+      timeout = 0;
       systemd-boot.enable = true;
-      systemd-boot.memtest86.enable = true;
-      efi.canTouchEfiVariables = false;
+      systemd-boot.memtest86.enable = false;
+      efi.canTouchEfiVariables = true;
     };
-    # plymouth = {
-    #   enable = true;
-    #   font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-    #   themePackages = [pkgs.catppuccin-plymouth];
-    #   theme = "catppuccin-macchiato";
-    # };
+    plymouth = {
+      enable = true;
+      font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+      themePackages = [ pkgs.catppuccin-plymouth ];
+      theme = "catppuccin-macchiato";
+    };
   };
 }
--- system/linux-kernel.nix
+++ system/linux-kernel.nix
@@ -1,29 +1,17 @@
 {pkgs, ...}: {
   # Linux Kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [
-    "quiet"
-    "fbcon=nodefer"
-    "vt.global_cursor_default=0"
-    "kernel.modules_disabled=1"
-    "lsm=landlock,lockdown,yama,integrity,apparmor,bpf,tomoyo,selinux"
-    "usbcore.autosuspend=-1"
-    "video4linux"
-    "acpi_rev_override=5"
-    "security=selinux"
-  ];
-  # boot.kernelPatches = [ {
-  #      name = "selinux-config";
-  #      patch = null;
-  #      extraConfig = ''
-  #              SECURITY_SELINUX y
-  #              SECURITY_SELINUX_BOOTPARAM n
-  #              SECURITY_SELINUX_DEVELOP y
-  #              SECURITY_SELINUX_AVC_STATS y
-  #              DEFAULT_SECURITY_SELINUX n
-  #            '';
-  # } ];
-
+  # boot.kernelParams = [
+  #   "quiet"
+  #   "fbcon=nodefer"
+  #   "vt.global_cursor_default=0"
+  #   "kernel.modules_disabled=1"
+  #   "lsm=landlock,lockdown,yama,integrity,apparmor,bpf,tomoyo,selinux"
+  #   "usbcore.autosuspend=-1"
+  #   "video4linux"
+  #   "acpi_rev_override=5"
+  #   "security=selinux"
+  # ];
   systemd.package = pkgs.systemd.override {withSelinux = true;};
 
   environment.systemPackages = with pkgs; [
