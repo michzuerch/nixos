7b59764e52f2c01900bc97236b9564fecbf9c0d9
Michael Zuercher <michzuerch@gmail.com>
2024-10-16 17:23:56 +0200

    anyrun

--- shell.nix
+++ shell.nix
@@ -20,9 +20,9 @@
       inherit
         (pkgs)
         # Required for pre-commit hook 'nixpkgs-fmt' only on Darwin
-
+        
         # REF: <https://discourse.nixos.org/t/nix-shell-rust-hello-world-ld-linkage-issue/17381/4>
-
+        
         nil
         alejandra
         nh
